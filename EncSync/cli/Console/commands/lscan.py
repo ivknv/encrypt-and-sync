b727f1cb06c44585d850998a64daa2988e1d406e
Ivan Konovalov <rvan.mega@gmail.com>
2017-05-29 20:01:50 +0500

    Remove useless argument

--- EncSync/cli/Console/commands/lscan.py
+++ EncSync/cli/Console/commands/lscan.py
@@ -11,10 +11,9 @@ def cmd_lscan(console, args):
     parser = argparse.ArgumentParser(description="Scan local directories",
                                      prog=args[0])
     parser.add_argument("dirs", nargs="+")
-    parser.add_argument("--n-workers", default=1, type=positive_int)
 
     ns = parser.parse_args(args[1:])
 
     env = Environment(console.env)
 
-    return do_scan(env, ["local://" + i for i in ns.dirs], ns.n_workers)
+    return do_scan(env, ["local://" + i for i in ns.dirs], 1)
