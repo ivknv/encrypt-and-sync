3c75a0fd4b5a6c070900688cea7f90c47245b692
Ivan Konovalov <rvan.mega@gmail.com>
2017-05-31 20:17:58 +0500

    Scrollable target & worker lists

--- EncSync/cli/TargetDisplay.py
+++ EncSync/cli/TargetDisplay.py
@@ -6,12 +6,63 @@ import threading
 
 from . import common
 
+class ScrollableList(object):
+    def __init__(self, values=None):
+        if values is None:
+            values = []
+
+        self.list = values
+        self._selection = 0
+        self._viewport_idx = 0
+        self._viewport_size = 0
+
+    @property
+    def selection(self):
+        return self._selection
+
+    @selection.setter
+    def selection(self, value):
+        self._selection = value % self.length
+
+        d = self._selection - self.viewport_idx
+
+        if d < 0:
+            self.viewport_idx = self._selection
+        elif d >= self.viewport_size - 1:
+            self.viewport_idx = self._selection - self.viewport_size + 1
+
+    @property
+    def viewport_idx(self):
+        return self._viewport_idx
+
+    @viewport_idx.setter
+    def viewport_idx(self, value):
+        self._viewport_idx = value % self.length
+
+    @property
+    def length(self):
+        return len(self.list)
+
+    @property
+    def viewport_size(self):
+        return min(self._viewport_size, self.length)
+
+    @viewport_size.setter
+    def viewport_size(self, value):
+        self._viewport_size = value
+
+    @property
+    def n_visible(self):
+        return min(self.viewport_size, self.length - self.viewport_idx)
+
 class TargetDisplay(object):
     def __init__(self, stdscr=None, target_manager=None):
         self.quit = self.force_quit = False
-        self.cur_target_idx = 0
         self.stdscr = stdscr
         self.targets = []
+        self.target_list = ScrollableList(self.targets)
+        self.worker_list = ScrollableList()
+        self.cur_list = self.target_list
         self.target_manager = target_manager
         self.manager_name = "<Target manager>"
         self.x = self.y = 0
@@ -42,18 +93,12 @@ class TargetDisplay(object):
         targets = self.targets
 
         if k == curses.KEY_UP:
-            if self.cur_target_idx == 0:
-                self.cur_target_idx = len(targets) - 1
-            else:
-                self.cur_target_idx -= 1
+            self.cur_list.selection -= 1
         elif k == curses.KEY_DOWN:
-            if self.cur_target_idx >= len(targets) - 1:
-                self.cur_target_idx = 0
-            else:
-                self.cur_target_idx += 1
+            self.cur_list.selection += 1
         elif k in (ord("s"), ord("r")):
             try:
-                target = targets[min(self.cur_target_idx, len(targets) - 1)]
+                target = targets[self.target_list.selection]
                 if target.status != "finished":
                     if k == ord("s"):
                         target.change_status("suspended")
@@ -75,6 +120,11 @@ class TargetDisplay(object):
         elif k == ord("Q"):
             self.quit = True
             self.force_quit = True
+        elif k == ord("\t"):
+            if self.cur_list is self.target_list:
+                self.cur_list = self.worker_list
+            else:
+                self.cur_list = self.target_list
         else:
             return
 
@@ -132,6 +182,7 @@ class TargetDisplay(object):
         return [str(idx + 1), target.path, str(target.status)]
 
     def display_target_info(self, oy=0, ox=0):
+        targets = self.targets
         header = self.target_columns
         rows = []
         colors = [None]
@@ -139,12 +190,18 @@ class TargetDisplay(object):
         x = self.x + ox
         y = self.y + oy
 
-        targets = self.targets
+        self.target_list.viewport_size = (self.stdscr.getmaxyx()[0] - y - 1) // 2
 
-        for target, i in zip(targets, range(len(targets))):
-            rows.append(self.get_target_columns(target, i))
+        a = self.target_list.viewport_idx
+        b = a + self.target_list.n_visible
 
-            colors.append(self.highlight_pair if i == self.cur_target_idx else None)
+        for i in range(a, b):
+            rows.append(self.get_target_columns(targets[i], i))
+
+            if self.cur_list is self.target_list and i == self.target_list.selection:
+                colors.append(self.highlight_pair)
+            else:
+                colors.append(None)
 
         height = common.display_table(y, x, self.stdscr, header, rows, colors)
 
@@ -155,6 +212,7 @@ class TargetDisplay(object):
 
         header = ["No.", "Progress", "Operation", "Path"]
         rows = []
+        colors = [None]
 
         x = self.x + ox
         y = self.y + oy
@@ -165,8 +223,14 @@ class TargetDisplay(object):
 
         y += 1
 
-        for worker, i in zip(workers, range(len(workers))):
-            info = worker.get_info()
+        self.worker_list.list = workers
+        self.worker_list.viewport_size = self.stdscr.getmaxyx()[0] - y
+
+        a = self.worker_list.viewport_idx
+        b = a + self.worker_list.n_visible
+
+        for i in range(a, b):
+            info = workers[i].get_info()
             path = info.get("path", None) or "N/A"
             operation = info.get("operation", None) or "N/A"
             progress = info.get("progress", None) or "N/A"
@@ -179,6 +243,11 @@ class TargetDisplay(object):
 
             rows.append([str(i + 1), progress, operation, path])
 
-        height += common.display_table(y, x, self.stdscr, header, rows)
+            if self.cur_list is self.worker_list and i == self.worker_list.selection:
+                colors.append(self.highlight_pair)
+            else:
+                colors.append(None)
+
+        height += common.display_table(y, x, self.stdscr, header, rows, colors)
 
         return height
