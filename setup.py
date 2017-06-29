ff6c80bde0194472229250431a8093f0ec39f7ef
Ivan Konovalov <rvan.mega@gmail.com>
2017-06-29 17:21:20 +0500

    Change version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name="EncSync",
-      version="0.1.0",
+      version="0.1.1",
       description="Yandex.Disk encrypted sync tool",
       author="Ivan Konovalov",
       packages=find_packages(),
