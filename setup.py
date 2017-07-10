aec13d9b0a59e2f9d6d84165960fc92f4d4a19bc
Ivan Konovalov <rvan.mega@gmail.com>
2017-07-10 17:32:09 +0500

    0.1.6 -> 0.1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.platform.startswith("win"):
     readline_pkg = "pyreadline"
 
 setup(name="EncSync",
-      version="0.1.6",
+      version="0.1.7",
       description="Yandex.Disk encrypted sync tool",
       author="Ivan Konovalov",
       packages=find_packages(),
