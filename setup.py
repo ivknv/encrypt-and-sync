497752e9ab97351af20bdac82ba31406f78f06f1
Ivan Konovalov <rvan.mega@gmail.com>
2017-10-30 17:14:18 +0500

    Update setup.py to exclude tests

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name="EncSync",
       version="0.1.11",
       description="Yandex.Disk encrypted sync tool",
       author="Ivan Konovalov",
-      packages=find_packages(),
+      packages=find_packages(exclude=["tests"]),
       install_requires=requirements,
       extras_require={"readline": [readline_pkg]},
       entry_points={"console_scripts": ["encsync=EncSync.__main__:main"]})
