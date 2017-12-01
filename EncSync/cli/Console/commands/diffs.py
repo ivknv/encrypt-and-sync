1eeeebde24fdc6b92698773d23b7434dd8141333
Ivan Konovalov <rvan.mega@gmail.com>
2017-12-01 23:37:50 +0500

    Fix DiffsCommand

--- EncSync/cli/Console/commands/diffs.py
+++ EncSync/cli/Console/commands/diffs.py
@@ -3,24 +3,17 @@
 
 import argparse
 
-from .... import Paths
 from ...show_diffs import show_diffs
 from ...Environment import Environment
-from ...common import recognize_path, local_path, non_local_path
 from ....EncScript import Command
 
 class DiffsCommand(Command):
     def evaluate(self, console):
         parser = argparse.ArgumentParser(description="Show differences",
                                          prog=self.args[0])
-        parser.add_argument("local", type=local_path)
-        parser.add_argument("remote", type=non_local_path)
+        parser.add_argument("target")
 
         ns = parser.parse_args(self.args[1:])
-
-        ns.local = recognize_path(ns.local)[0]
-        ns.remote = Paths.join_properly(console.cwd, recognize_path(ns.remote)[0])
-
         env = Environment(console.env)
 
-        return show_diffs(env, ns.local, ns.remote)
+        return show_diffs(env, ns.target)
