9f6b4578710117a80dbd25946d31441863f5be94
Ivan Konovalov <rvan.mega@gmail.com>
2018-02-04 14:14:03 +0500

    0.2.0 -> 0.3.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ else:
     scripts = []
 
 setup(name="EncSync",
-      version="0.2.0",
+      version="0.3.0",
       description="Yandex.Disk encrypted sync tool",
       author="Ivan Konovalov",
       packages=find_packages(exclude=["tests"]),
