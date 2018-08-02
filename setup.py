b00404c978df847861ec3fbf73819a8c7315162c
Ivan Konovalov <ivknv0@gmail.com>
2018-08-02 16:42:52 +0500

    Update setup.py, add MANIFEST.in, rename LICENSE.txt -> COPYING

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import codecs
 import os
 import sys
 
@@ -27,15 +28,41 @@ else:
 if os.environ.get("USE_PYCRYPTODOMEX", None):
     requirements[requirements.index("pycryptodome")] = "pycryptodomex"
 
+module_dir = os.path.dirname(__file__)
+
+with codecs.open(os.path.join(module_dir, "README.rst"), encoding="utf8") as f:
+    long_description = f.read()
+
 setup(name="eas",
       version="0.7.2",
-      description="A file synchronization utility with client-side encryption support",
+      description="Encrypt & Sync is a file synchronization utility with client-side encryption support",
+      long_description=long_description,
+      url="https://encryptandsync.com",
+      project_urls={"Website":       "https://encryptandsync.com",
+                    "Source code":   "https://github.com/ivknv/encrypt-and-sync",
+                    "Documentation": "https://encryptandsync.com/docs",
+                    "Bug tracker":   "https://github.com/ivknv/encrypt-and-sync/issues",
+                    "Donate":        "https://paypal.me/encryptandsync"},
       author="Ivan Konovalov",
+      author_email="ivknv0@gmail.com",
       license="GPLv3",
       packages=find_packages(exclude=["tests"]),
       install_requires=requirements,
+      python_requires=">=3.5",
       extras_require={"yandex.disk": ["yadisk>=1.2.5", "requests"],
                       "dropbox":     ["dropbox", "requests"],
                       "sftp":        ["pysftp", "paramiko"]},
       entry_points=entry_points,
-      scripts=scripts)
+      scripts=scripts,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: End Users/Desktop",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+          "Topic :: Internet",
+          "Topic :: Utilities"],
+      keywords="sync encryption dropbox yandex sftp")
